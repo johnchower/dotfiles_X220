4dcd7317b50d73c5b901a66385ed72e69c0babdd
John Hower <johnchower@gmail.com>
2016-12-24 21:20:31 -0700

    Exclude symlinks to swap files

--- link_finder.sh
+++ link_finder.sh
@@ -1,1 +1,1 @@
-sudo find . -type l -ls | grep dotfiles | grep '>' | awk '{$1=$2=$3=$4=$5=$6=$7=$8=$9=$10=""; print $0}' > ~/dotfiles_X220/link_targets
+sudo find . -type l -ls | grep dotfiles | grep '>' | grep -v proc | awk '{$1=$2=$3=$4=$5=$6=$7=$8=$9=$10=""; print $0}' > ~/dotfiles_X220/link_targets
